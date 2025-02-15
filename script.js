ef747d50529000cf4e06e22d758b14c5a6788282
ITLEGO <mokshev.savva@gmail.com>
2025-02-15 17:35:10 +0300

    переключатьЗаголовок сделали

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 console.log("Hallo world")
-let xxx =  31235 * 2/100 - 57
-console.log (xxx)
+let xxx = 31235 * 2 / 100 - 57
+console.log(xxx)
 
 let yyy = Math.sqrt(8)
 // Math.sqrt(17) 
 // Math.sin(2) 
 
-console.log("Корень из 8 = " , yyy)
+console.log("Корень из 8 = ", yyy)
 console.log("Вася")
 console.log(100 + 2)
 console.log("100" + 2)
-let имя="Савва"
-let возрост=333
-console.log("мня зовут", имя, "и мне",возрост, "лет")
-console.log("мня зовут", имя, "и мне",возрост, "лет")
-console.log("мня зовут " + имя, "и мне",возрост, "лет")
-console.log(`мня зовут ${имя} и мне ${ возрост} лет`)
+let имя = "Савва"
+let возрост = 333
+console.log("мня зовут", имя, "и мне", возрост, "лет")
+console.log("мня зовут", имя, "и мне", возрост, "лет")
+console.log("мня зовут " + имя, "и мне", возрост, "лет")
+console.log(`мня зовут ${имя} и мне ${возрост} лет`)
 
 
 
@@ -26,17 +26,47 @@ console.log(`мня зовут ${имя} и мне ${ возрост} лет`)
 
 
 // придумали переменную "кнопка" которая ведет на кнопку "B1"
-let кнопка = document.querySelector(".B1")
+let центральнаяКнопка = document.querySelector(".B1")
+let верхняяКнопка = document.querySelector(".Рамка")
 
 // делает так что нажатие на "кнопка" вызывает команду сменитьЦветШтуки() 
-кнопка.addEventListener("click", function(){
-    сменитьЦветШтуки()   
+центральнаяКнопка.addEventListener("mousedown", function () {
+    переключитьЗаголовок()
+})
+верхняяКнопка.addEventListener("mousedown", function () {
+    сменитьЦветЛоготипа()
 })
 
-// новая функция -- меняет backgroundColor .заголовок-секции на "yellow"
-function сменитьЦветШтуки(){
-    document.querySelector(".заголовок-секции").style.backgroundColor = "yellow"
+let цветЗаголовка = "бежевый"
+function переключитьЗаголовок(){
+    
+    if(цветЗаголовка == "бежевый"){
+          document.querySelector(".заголовок-секции").style.backgroundColor = "green"
+          цветЗаголовка = "зеленый"
+    }
+    else{
+          document.querySelector(".заголовок-секции").style.backgroundColor = "rgb(255, 242, 222, 1)"
+          цветЗаголовка = "бежевый"
+    }
 }
+
+// новая функция -- меняет backgroundColor .заголовок-секции на "yellow"
+function сменитьЗаголовокСекции() {
+    document.querySelector(".заголовок-секции").style.backgroundColor = "green"
+}
+function сменитьЦветЛоготипа() {
+    document.querySelector(".Город").style.backgroundColor = "green"
+}
+function сменитьЦветШтуки3() {
+    document.querySelector(".Главное-лого").style.color = "green"
+}
+
+
+
+
+
+
+
 
 // сделать так чтоб цвет менялся на зеленый
 // сделать чтоб была команда "сменитьЦветЛого()" -- меняет цвет логотипа на другой
